6cfffc80290db35dd930140dda9fe332c07ad097
Chayatan Angkasuvan <49066536+chaya-tan@users.noreply.github.com>
2021-09-25 15:11:16 +0700

    connect firestore

---
+++ public/main.js
@@ -0,0 +1,8 @@
+let productCode = "หห";
+let productVariant = "หหฟ้า หหแดง หหชมพู";
+let productPrice = "120";
+let productIndex = 0;
+
+const setProductDetail = (productIndex) => {};
+
+setProductDetail(1);
